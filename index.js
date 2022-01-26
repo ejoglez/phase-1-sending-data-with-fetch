769b3e3371fc3569f6334ec93490c132c85d8d1a
Eric Gonzalez <ejoglez@gmail.com>
2022-01-26 11:26:51 -0600

    commit tree 4bc8c5f6b6467708e2aa3d886673e97e475b95d9

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add your code here
+// POST 
+function submitData(){
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: '',
+            email: '',
+        })     
+    })
+}
